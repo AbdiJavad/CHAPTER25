03ff9290461bd0e21b59c4e9dc6381a06730f526
AbdiJavad <Mirjavadabdi@gmail.com>
2023-11-05 19:23:16 +0330

    first commit

---
+++ src/Heap/Heap.java
@@ -0,0 +1,14 @@
+package Heap;
+
+public class Heap<T> {
+    public void add(T tree) {
+    }
+
+    public T remove() {
+        return null;
+    }
+
+    public boolean getSize() {
+        return false;
+    }
+}
